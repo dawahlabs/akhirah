2107813f870c3f81379667a640f69da97dbf869f
fayaz <fayazp088@gmail.com>
2025-03-25 01:32:41 +0530

    testing the cd flow.

--- apis/services/akhirah/main.go
+++ apis/services/akhirah/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World! This is the Akhirah.")
+	fmt.Fprintln(w, "Finally the setup is done.")
 }
 
 func main() {
