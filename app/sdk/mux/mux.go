cf88191f4d322c43cb7669cc070c018ac34fd643
fayaz <fayazp088@gmail.com>
2025-03-28 00:36:05 +0530

    Adds erorr handling.

--- app/sdk/mux/mux.go
+++ app/sdk/mux/mux.go
@@ -12,7 +12,7 @@ import (
 )
 
 func WebAPI(shutdown chan os.Signal, log *logger.Logger) *web.App {
-	mux := web.NewApp(shutdown, mid.Logger(log))
+	mux := web.NewApp(shutdown, mid.Logger(log), mid.Errors(log))
 
 	checkapp.Routes(mux)
 
